953dd80359c812343082bf7dcf7cadbfd578fc3b
iisadiq90 <40710641+iisadiq90@users.noreply.github.com>
2018-09-14 19:37:48 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,7 +17,19 @@ client.on('message', message => {
 
 
 
+client.on("message", message => {
 
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`  عضو \`${message.guild.members.filter(m => m.presence.status !== 'all').size}\` : تم الأرسال ل`); 
+ message.delete(); 
+};     
+});
 
 
 

2624ce338dbc52136b3435fe287279c391d98d39
iisadiq90 <40710641+iisadiq90@users.noreply.github.com>
2018-09-14 19:00:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
- const prefix = "#";
+const prefix = "!";
+var prefix = '!';
 client.on('ready', () => {
     console.log('I am ready!');
 });
@@ -10,6 +11,55 @@ client.on('message', message => {
         message.reply('s');
       }
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.on('message', message => {
+if (message.author.id === client.user.id) return;
+if (message.guild) {
+let embed = new Discord.RichEmbed()
+let args = message.content.split(' ').slice(1).join(' ');
+if(message.content.split(' ')[0] == prefix + '!bc') {
+if(!message.channel.guild) return message.reply('** وت ار يو دونك ؟ هذا الأمر فقط داخل السيرفر **');         
+if (!args[1]) {
+return;
+}
+  message.guild.members.forEach(m => {
+if(!message.member.hasPermission('ADMINISTRATOR')) return;
+      var bc = new Discord.RichEmbed()
+      .addField('# | الرسالة ', args)
+      .setThumbnail(message.guild.iconURL)
+      .setColor('RANDOM')
+      m.sendMessage(args)
+  });
+         if(!message.member.hasPermission('ADMINISTRATOR')) return message.reply("** #shop انت لست ادمن في السيرفر يمكنك شراء الادمن من**");
+  const AziRo = new Discord.RichEmbed()   
+  .setColor('RANDOM')
+  message.channel.sendEmbed(AziRo);          
+}
+} else {
+  return;
+}
+});
+
+
+
+
+
+
+
+
 
 
 

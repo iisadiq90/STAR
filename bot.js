7555ea37154f92655c14517c1d8ef1b1b4a0dced
iisadiq90 <40710641+iisadiq90@users.noreply.github.com>
2018-09-14 19:42:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,7 +19,7 @@ client.on('message', message => {
 
 client.on("message", message => {
 
-            if (message.content.startsWith(prefix + "bc")) {
+            if (message.content.startsWith("!bc")) {
                          if (!message.member.hasPermission("ADMINISTRATOR"))  return;
   let args = message.content.split(" ").slice(1);
   var argresult = args.join(' '); 

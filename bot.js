c5f40ce64119f801aeb646e2ea970ebdfa3acd63
iisadiq90 <40710641+iisadiq90@users.noreply.github.com>
2018-09-18 19:10:27 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,7 +27,7 @@ if (message.content.startsWith(prefix + "ban")) {
 
     mention.ban("By: " + message.author.tag);
     
-    message.channel.send("تم أعطاء باند الى : " + mention.tag);
+    message.channel.send("تم أعطاء باند الى : " + message.mentions.members());
 };
 });
 
@@ -38,7 +38,7 @@ if (message.content.startsWith(prefix + "unban")) {
 
     mention.unban("By: " + message.author.tag);
     
-    message.channel.send("تم ألغاء الباند : " + mention.tag);
+    message.channel.send("تم ألغاء الباند : " + message.mentions.members());
 };
 });
 

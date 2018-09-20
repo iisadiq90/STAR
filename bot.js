6f1cc343aa2c053b6365488b2c5ed5c2a0a00d5d
iisadiq90 <40710641+iisadiq90@users.noreply.github.com>
2018-09-20 23:22:45 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === '#s') {
+    if (message.content === '@s') {
         message.reply('yas');
       }
 });
@@ -190,34 +190,11 @@ message.channel.send({embed});
 
 
 
-  client.on("message", async message => {
-  if(message.author.bot) return;
-  if(message.channel.type === "dm") return;
-  let prefix = botconfig.prefix;
-  let messageArray = message.content.split(" ");
-  let cmd = messageArray.slice(1);
-
-if(cmd === `${prefix}server`);
-let sicon = message.guild.displyAvatarURL;
-let serverEmbed = new Discord.RichEmbed()
-.setDescription("``Server Information``")
-.addField("Server Name", message.guild.name)
-.addField("Created On", message.guild.createdAt)
-.addField("You Joined", message.member.joinedAt)
-.addField("Total Members", message.guild.memberCount)
-.addField("Server Owner", message.guild.owner.user.username)
-.addField("Rooms", message.guild.channels.size)
-.addField("Roles", message.guild.roles.size)
-.addField("Region", message.guild.region)
-.addField("Server ID", message.guild.id);
-
-return message.channel.send(serverEmbed);
-});
-  
 
 
 
-/*
+
+
 
 client.on('message', function(msg) {
     if(msg.content.startsWith (prefix  + 'server')) {
@@ -239,7 +216,7 @@ client.on('message', function(msg) {
   });
 
 
-*/
+
 
 client.on('message', message => {
 

8e9077349be0b9c266a6335d5beecffb713589ac
iisadiq90 <40710641+iisadiq90@users.noreply.github.com>
2018-09-20 22:40:19 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -326,27 +326,18 @@ client.on('message', msg => {
     let textxt = args.slice(0).join("");
     if(msg.member.hasPermission("MANAGE_MESSAGES")) {
     if (textxt == "") {
-        msg.delete().then
-    msg.channel.send("***```ضع عدد الرسائل التي تريد مسحها 👌```***").then(m => m.delete(2500));
-} else {
     msg.delete().then
     msg.delete().then
     msg.channel.bulkDelete(textxt);
         msg.channel.send("```php\nعدد الرسائل التي تم مسحها: " + textxt + "\n```").then(m => m.delete(2500));
+} else {
+ 
+        msg.delete().then
+    msg.channel.send("***```ضع عدد الرسائل التي تريد مسحها 👌```***").then(m => m.delete(2500));
         }    
     }
 }
 });
-
-
-
-
-
-
-
-
-
-
 
 
 

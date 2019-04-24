36a39684641b2a945a30bb905896c68fef7d0d15
iisadiq90 <40710641+iisadiq90@users.noreply.github.com>
2019-04-24 17:18:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,30 +22,55 @@ channel.send(`1ArTIcdreamskdas31231ArTIcdreamskdas31231ArTIcdreamskdas31231ArTIc
 
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+
+  console.log(Logged in as ${client.user.tag}!);
+
 });
+
  
+
 client.on('message', message => {
+
    
+
     let args = message.content.split(' ').slice(1).join(' ');
+
    
+
   if (message.content === 'ping') {
-      message.channel.send(`<@${message.author.id}> Ping..!`)
+
+      message.channel.send(<@${message.author.id}> Ping..!)
+
   }
+
  
+
  
-  if (message.content.startsWith('.bc')) {
+
+  if (message.content.startsWith('$bc')) {
+
           if (!args[0]) {
-message.channel.send("**اكتب شي**");
+
+message.channel.send("$bc <message>");
+
 return;
+
 }
+
 message.guild.members.forEach(m => {
+
    if(!message.member.hasPermission('ADMINISTRATOR')) return;
-   m.send(${args} ${m});
+
+   m.send('${args} ${m}');
+
  
+
 });
+
   }
+
  
+
 });
 
 

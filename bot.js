150308c4fb7fd90ad65301956e1b7b7805fd1eb3
iisadiq90 <40710641+iisadiq90@users.noreply.github.com>
2019-11-23 14:41:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -231,12 +231,12 @@ client.on('message', message=> {
 
 var config = {
   events: [
-    {type: "CHANNEL_CREATE", logType: "CHANNEL_CREATE", limit: 2, delay: 5000},
-    {type: "CHANNEL_DELETE", logType: "CHANNEL_DELETE", limit: 2, delay: 5000},
-    {type: "GUILD_MEMBER_REMOVE", logType: "MEMBER_KICK", limit: 4, delay: 5000},
-    {type: "GUILD_BAN_ADD", logType: "MEMBER_BAN_ADD", limit: 2, delay: 5000},
-    {type: "GUILD_ROLE_CREATE", logType: "ROLE_CREATE", limit: 2, delay: 5000},
-    {type: "GUILD_ROLE_DELETE", logType: "ROLE_DELETE", limit: 2, delay: 5000},
+    {type: "CHANNEL_CREATE", logType: "CHANNEL_CREATE", limit: 2, delay: 10000},
+    {type: "CHANNEL_DELETE", logType: "CHANNEL_DELETE", limit: 2, delay: 10000},
+    {type: "GUILD_MEMBER_REMOVE", logType: "MEMBER_KICK", limit: 4, delay: 10000},
+    {type: "GUILD_BAN_ADD", logType: "MEMBER_BAN_ADD", limit: 2, delay: 10000},
+    {type: "GUILD_ROLE_CREATE", logType: "ROLE_CREATE", limit: 2, delay: 10000},
+    {type: "GUILD_ROLE_DELETE", logType: "ROLE_DELETE", limit: 2, delay: 10000},
   ]
 }
 client.on("error", (e) => console.error(e));
